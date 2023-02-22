f0a394c33305b9ea75cf3edbf793018870ffe9c1
Manvir Mann <manvirm29@gmail.com>
2023-02-21 23:38:45 -0800

    Load data and preprocess

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+# Load data and split into training and testing
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Preprocessing
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, aixs = 1)
