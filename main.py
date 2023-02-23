f99863b2979a4d7a5bbd9ed6752f6f89f08bd5e7
Manvir Mann <manvirm29@gmail.com>
2023-02-22 23:08:32 -0800

    Build and compile neural network

--- main.py
+++ main.py
@@ -10,4 +10,16 @@ mnist = tf.keras.datasets.mnist
 
 # Preprocessing
 x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, aixs = 1)
+x_test = tf.keras.utils.normalize(x_test, axis = 1)
+
+# Create Neural Network Model
+model = tf.keras.models.Sequential()
+# Input layer
+model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+# Hidden layers
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+# Output layer (10 digits)
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
